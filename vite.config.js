076cdb13b6ac303bf6e96928b2237f5a9f794d03
Cesar Sevayos <cesarsevayos@gmail.com>
2025-10-02 15:49:24 -0300

    configuracion para gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/app-busquedas-baires/", // 👈 IMPORTANTE para GH Pages
+});
