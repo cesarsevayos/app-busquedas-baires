4f4b116cb484f85597157b952a76996d98ad9588
Cesar Sevayos <cesarsevayos@gmail.com>
2025-10-02 17:30:18 -0300

    Camnbios

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
-import { HashRouter } from "react-router-dom"; // 👈 agregado
 import App from "./App";
 import "./index.css";
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log("CLERK KEY:", clerkPubKey);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkPubKey}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </ClerkProvider>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/app-busquedas-baires/", // 👈 IMPORTANTE para GH Pages
-});
+  server: {
+    allowedHosts: [
+      'pseudozoological-barrable-filomena.ngrok-free.dev'
+    ],
+  },
+})
